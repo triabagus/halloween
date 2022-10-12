64a93d2290bca6b5da06e2ee045537d47d39bec9
triabagus <triatop9@gmail.com>
2022-10-12 21:16:10 +0700

    header & menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,33 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToogle = document.getElementById('nav-toogle'),
+    navClose  = document.getElementById('nav-close');
 
+    // Menu Show
+    // validate if constant exists
+    if(navToogle){
+        navToogle.addEventListener('click', () =>{
+            navMenu.classList.add('show-menu')
+        })
+    }
+
+    // Menu Hide
+    // validate if constant exists
+    if(navClose){
+        navClose.addEventListener('click', () =>{
+            navMenu.classList.remove('show-menu')
+        })
+    }
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
 
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // when we click on each nav_link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
